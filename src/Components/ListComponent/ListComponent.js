b363d5d8fd5c5f7a4c91948ef5065006b4b128f7
Jeff Soriano <soriano.jeffm@gmail.com>
2020-11-07 16:26:39 -0800

    Add delete icon to list item

--- src/Components/ListComponent/ListComponent.js
+++ src/Components/ListComponent/ListComponent.js
@@ -1,11 +1,24 @@
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
 
 import InputArea from "./InputArea"
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: 280,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
 export default function ListComponent() {
+    const classes = useStyles();
     const [listItems, setListItems] = useState([]);
 
     function addListItem(item) {
@@ -13,7 +26,7 @@ export default function ListComponent() {
     }
 
     return (
-        <div>
+        <div className={classes.root}>
             <InputArea onSubmit={addListItem} />
             <List>
                 {listItems.map(item => {
@@ -22,6 +35,11 @@ export default function ListComponent() {
                             <ListItemText
                                 primary={item}
                             />
+                            <ListItemSecondaryAction>
+                                <IconButton>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </ListItemSecondaryAction>
                         </ListItem>
                     )
 
